9e08ad52b8754c7105918728fc5da777a004cca9
luong <luong@DESKTOP-GDSR1P3>
2024-11-07 21:56:57 +0700

    Add project files.

---
+++ Harmic/ViewComponents/BlogViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Harmic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Harmic.ViewComponents
+{
+    public class BlogViewComponent : ViewComponent
+    {
+        private readonly HamicContext _context;
+
+        public BlogViewComponent(HamicContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.TbBlogs
+                .Where(b => (bool)b.IsActive == true)
+                .OrderByDescending(b => b.BlogId);
+
+            return await Task.FromResult<IViewComponentResult>
+                (View(items.ToList())
+            );
+        }
+    }
+}
---
+++ Harmic/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Harmic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Harmic.ViewComponents
+{
+	public class ProductViewComponent : ViewComponent
+	{
+		private readonly HamicContext _context;
+
+		public ProductViewComponent(HamicContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var items = _context.TbProducts.Include(m => m.CategoryProduct)
+				.Where(m => (bool)m.IsActive).Where(m => m.IsNew);
+			return await Task.FromResult<IViewComponentResult>
+				(View(items.OrderByDescending(m => m.ProductId).ToList()));
+		}
+	}
+}
